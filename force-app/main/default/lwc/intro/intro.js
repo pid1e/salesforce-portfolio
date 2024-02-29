ebe44d0958cabe431ea006bcbadfd8bcd11d9a87
Priyadarshini Ghosh <priya@Priyadarshinis-MacBook-Air.local>
2024-02-28 22:37:22 -0500

    Added intro section

---
+++ force-app/main/default/lwc/intro/intro.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import IMAGES from '@salesforce/resourceUrl/Images'
+
+export default class Intro extends LightningElement {
+    introImage = IMAGES + '/intro-pic.jpeg';
+}
