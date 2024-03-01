7f010ef843f6c51b24f138fa09c8e4e99d99b16f
Priyadarshini Ghosh <priya@Priyadarshinis-MacBook-Air.local>
2024-03-01 17:46:11 -0500

    Added myProject cards

---
+++ force-app/main/default/lwc/myProjects/myProjects.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+import IMAGES from '@salesforce/resourceUrl/Images'
+
+export default class SkillCards extends LightningElement {
+    project1ImageURL = IMAGES + '/Project-1 (1).png';
+    project2ImageURL = IMAGES + '/project2-logo.png';
+    project3ImageURL = IMAGES + '/Project-3.png';
+    lntImageURL = IMAGES + '/Lntlogo.jpeg';
+    
+}
