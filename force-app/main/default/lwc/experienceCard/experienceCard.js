15beab3b97194512e857eda08b58e39ae98714e2
Priyadarshini Ghosh <priya@Priyadarshinis-MacBook-Air.local>
2024-02-29 22:09:59 -0500

    Added experienceCard component

---
+++ force-app/main/default/lwc/experienceCard/experienceCard.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+import IMAGES from '@salesforce/resourceUrl/Images'
+
+export default class SkillCards extends LightningElement {
+    wayfairImageURL = IMAGES + '/wayfair-logo.jpeg';
+    capgeminiImageURL = IMAGES + '/Cap-logo.jpeg';
+    atombergImageURL = IMAGES + '/atom-logo.png';
+    lntImageURL = IMAGES + '/Lntlogo.jpeg';
+    
+}
